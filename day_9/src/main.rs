fafc5640402132723dc44d61b03f9c10d8abfccb
Matthew Millar <mattymillar@gmail.com>
2023-12-12 18:20:37 +0000

    day 9 part 2

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -4,39 +4,46 @@ fn diff_sequence(parent: &Vec<i32>) -> Vec<i32> {
     parent.windows(2).map(|pair| pair[1] - pair[0]).collect()
 }
 
-fn all_zero(sequence: &Vec<i32>) -> bool {
-    sequence.iter().all(|&x| x == 0)
+fn create_sequences(line: String) -> Vec<Vec<i32>> {
+    let mut sequences = vec![line
+        .split(" ")
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<_>>()];
+
+    loop {
+        let last = sequences.last().unwrap();
+
+        if last.iter().sum::<i32>() == 0 {
+            break;
+        }
+
+        sequences.push(diff_sequence(last));
+    }
+
+    sequences
 }
 
 fn part_1() -> i32 {
     load_lines("input.txt")
         .map(|line| {
-            let mut sequences = Vec::new();
-
-            let next_sequence = line
-                .split(" ")
-                .map(|n| n.parse::<i32>().unwrap())
-                .collect::<Vec<_>>();
-
-            sequences.push(next_sequence);
-
-            loop {
-                let last = sequences.get(sequences.len() - 1).unwrap();
-
-                if all_zero(last) {
-                    break;
-                }
-
-                sequences.push(diff_sequence(last));
-            }
-
-            sequences.iter().map(|v| v.last().unwrap()).sum::<i32>()
+            create_sequences(line)
+                .iter()
+                .map(|v| v.last().unwrap())
+                .sum::<i32>()
         })
         .sum()
 }
 
 fn part_2() -> i32 {
-    0
+    load_lines("input.txt")
+        .map(|line| {
+            create_sequences(line)
+                .iter()
+                .rev()
+                .skip(1)
+                .fold(0, |acc, next| next.first().unwrap() - acc)
+        })
+        .sum()
 }
 
 fn main() {
